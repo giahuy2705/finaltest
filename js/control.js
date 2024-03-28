b65ca5539474075b0d6de499e3636f80e5633407
giahuy2705 <164825940+giahuy2705@users.noreply.github.com>
2024-03-28 16:13:05 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,25 @@
+// JavaScript Document$('#slide').owlCarousel({
+$('#slide').owlCarousel({
+loop:true,	// cho lap lại
+	items:1,	// xuất hiện 1 ảnh thôi
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+nav:true,	// cho xuất hiện bộ nút tới lui
+dot:true,	
+})
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
